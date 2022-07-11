4061b9b17b992a2e41a04b587f335099bfd518b8
ikncode <ajkaen863@gmail.com>
2022-07-11 14:00:39 +0300

    Add delete row func

--- app.py
+++ app.py
@@ -29,11 +29,16 @@ class App(QDialog):
         self.ds = QSqlTableModel()
         self.ds.setEditStrategy(QSqlTableModel.OnFieldChange)
         self.ds.setTable('Agent')
-        self.ds.removeColumn(0)
+
         self.ds.select()
         self.v = QTableView(selectionBehavior=QAbstractItemView.SelectRows)
         self.v.setModel(self.ds)
         self.v.resizeColumnsToContents()
+
+        self.NewIndex = self.v.model().index(self.ds.rowCount() - 1, 0)
+        self.maxrow = self.v.model().data(self.NewIndex)
+
+        self.v.hideColumn(0)
 
 
         #self.ds.insertRows(self.ds.rowCount(), 1)
@@ -44,25 +49,47 @@ class App(QDialog):
         self.plus_button.move(50,50)
         self.save_button = QPushButton(self.tr("Save"), self)
         self.save_button.clicked.connect(self.refresh)
+        self.delete_button = QPushButton(self.tr("Delete"), self)
+        self.delete_button.clicked.connect(self.delete_data)
         hbox.addWidget(self.v)
         vbox.addWidget(self.plus_button)
         vbox.addWidget(self.save_button)
+        vbox.addWidget(self.delete_button)
         hbox.addLayout(vbox)
 
         self.newadd = QAction('&New',self)
         self.newadd.setShortcut('Ctrl+N')
         self.newadd.triggered.connect(self.accept)
 
-
+    def keyPressEvent(self, event):
+        if event.key() in (Qt.Key_Backspace, Qt.Key_Delete):
+            self.delete_data()
+        QDialog.keyPressEvent(self, event)
 
     def initGUI(self):
         self.setWindowTitle("Balls")
-        self.setGeometry(300, 300, 1050, 1000)
+        self.setGeometry(300, 300, 1000, 500)
         self.show()
 
     @pyqtSlot()
     def add_data(self):
+        record = self.ds.record()
+        record.setGenerated('Id_agent', False)
         self.ds.insertRow(self.ds.rowCount())
+
+    @pyqtSlot()
+    def delete_data(self):
+            if self.v.selectionModel().hasSelection():
+                indexes = [QPersistentModelIndex(index) for index in self.v.selectionModel().selectedRows()]
+                print(self.maxrow)
+                next_ix = QPersistentModelIndex(self.ds.index(self.maxrow + 1, 0))
+                for index in indexes:
+                    print('Deleting row %d...' % index.row())
+                    self.ds.removeRow(index.row())
+
+                self.v.setCurrentIndex(QModelIndex(next_ix))
+            else:
+                print('No row selected!')
 
     @pyqtSlot()
     def refresh(self):
